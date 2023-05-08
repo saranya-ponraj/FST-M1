0bf6329ca046a4dedcfb6e62501040a26378ca5d
saranya-ponraj <132567730+saranya-ponraj@users.noreply.github.com>
2023-05-08 09:37:53 +0530

    Add files via upload

---
+++ JUnit/Activities/Activity2.java
@@ -0,0 +1,27 @@
+package Activities_junit;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+ class ExpectedExceptionTest {
+
+     @Test
+ void notEnoughFunds() {
+
+     BankAccount account = new BankAccount(9);
+
+
+         assertThrows(NotEnoughFundsException.class, () -> account.withdraw(10),
+             "Balance must be greater than amount of withdrawal");
+ }
+
+     @Test
+     void enoughFunds() {
+
+         BankAccount account = new BankAccount(100);
+
+
+         assertDoesNotThrow(() -> account.withdraw(100));
+     }
+
+}
